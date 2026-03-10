7828036dc40cb82046aab48ac02a7122578896f7
Joshua Ssenyonjo <joshuassenyonjo1@gmail.com>
2026-03-10 21:28:46 +0300

    InternshipPlacement.py

--- weekly_log_basic.py
+++ weekly_log_basic.py
@@ -1,4 +1,11 @@
-
+from django.db import models
+class InternshipPlacement(models.Model):
+  student = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name = "student_placements")
+  workplace_supervisor = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name = "workplace_supervisions")
+  academic_supervisor = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name = "academic_supervisions")
+  organisation = models.CharField(max_length = 200)
+  start_date = models.DateField()
+  end_date = models.DateField()
 
 
 
