17b1fac034bce8d2917e46dd9d55be32ee223a6b
Bryant <oketayotbrian810@gmail.com>
2026-03-11 10:47:56 +0300

    evaluation model

--- weekly_log_basic.py
+++ weekly_log_basic.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 class Evaluation(models.Model):
+  placement = models.ForeignKey(InternshipPlacement, on_delete = models.CASCADE)
+  evaluator = models.Foreignkey(CustomUser, on_delete = models.CASCADE)
+  criteria = models.ForeignKey(EvaluationCriteria, on_delete= models.CASCADE)
+  score = models.IntegerField(help_text = "Out of 100%")
+  date = models.DateField(null = True)
+
+def __str__(self):
+  pass
+  
+  
     
   
 
