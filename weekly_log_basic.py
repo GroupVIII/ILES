98999e4fa84fb2481d2ec89db9c9b70e5a71cd23
AROMORACH <aromorachritah@gmail.com>
2026-03-11 09:43:02 +0300

    Update weekly_log_basic.py

--- weekly_log_basic.py
+++ weekly_log_basic.py
@@ -1,5 +1,15 @@
+# 4. Evaluation criteria
+class EvaluationCriteria (models.model):
+  name = models.charField(max_length=100)
+  weight = models.IntegerField(help_text="percentage weight, e.g 40 for 40%)
 
+  def __str__(self):
+    return f"{self.name} ({self.weight}%)
+  
 
+                               
+
+                               
 
 
 
