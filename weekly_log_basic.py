9e73546d1adc682604745e8a26309e2d8fc56a8b
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-03-11 09:52:58 +0300

    added validation before saving placement

--- weekly_log_basic.py
+++ weekly_log_basic.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.core.exceptions import PermissionDenied 
 class InternshipPlacement (models.Model):pass 
-
+class InternshipPlacement(models.Model):
+  def save(self,*args,**kwargs):
+    self.full_clean()
+    super().save(*args,**kwargs)
 
 
 
