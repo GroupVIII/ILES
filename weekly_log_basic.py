12e2b63bf37661a2d879f99ae0ef84e29a7f123c
CHONO <chonoanzoajosephine@gmail.com>
2026-03-11 10:34:21 +0300

    Update weekly_log_basic.py

--- weekly_log_basic.py
+++ weekly_log_basic.py
@@ -1,4 +1,17 @@
 # Customer User Model
+# Parent class
+class User:
+  def __init__(self, username,password):
+    self.username = username
+    self.password = password
+  def login(self, username,password):
+       if self.username == username and self.password == password:
+            return True
+       return False
+
+# Student class
+class Student(User):
+  def student_dashboard(self):
 
 
 
