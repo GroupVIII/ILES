3445edfde132597d2316d27aa944b596a251718c
CHONO <chonoanzoajosephine@gmail.com>
2026-03-11 10:12:28 +0300

    Update weekly_log_basic.py

--- weekly_log_basic.py
+++ weekly_log_basic.py
@@ -1,10 +1,4 @@
-from django.db import models
-from django.core.exceptions import PermissionDenied 
-class InternshipPlacement (models.Model):pass 
-class InternshipPlacement(models.Model):
-  def save(self,*args,**kwargs):
-    self.full_clean()
-    super().save(*args,**kwargs)
+
 
 
 
