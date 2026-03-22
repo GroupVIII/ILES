49950f7c3914b4ab60c6383ec6b2168cfd0d66d3
Bryant <oketayotbrian810@gmail.com>
2026-03-22 21:32:59 +0300

    evaluation model

--- weekly_log_basic.py
+++ weekly_log_basic.py
@@ -1,5 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class CustomUser(models.Model):
+  username = models.CharField(max_length=150)
+  email = models.EmailField(unique=True)
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.username
+
+class InternshipPlacement(models.Model):
+  student = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+  company_name = models.CharField(max_length=255)
+  start_date = models.DateField()
+  end_date = models.DateField()
+
+  def __str__(self):
+    return f"{self.student.username} at {self.company_name}"
+
+# class IntrenshipLog(models.Model):
 class Evaluation(models.Model):
   placement = models.ForeignKey(InternshipPlacement, on_delete = models.CASCADE)
   evaluator = models.Foreignkey(CustomUser, on_delete = models.CASCADE)
@@ -8,7 +28,7 @@ class Evaluation(models.Model):
   date = models.DateField(null = True)
   class Meta:
     model = Evaluation 
-    fields = 
+    #fields = 
 
   def __str__(self):
 
