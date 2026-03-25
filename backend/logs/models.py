724d6b12c1b3e4d6d608d1dc0cec5c00c6cf7c40
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-03-25 12:46:26 +0300

    added status choices for weeklyLog

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Weeklylog(models.Model):
+   STATUS_CHOICES = ( 
+      ('DRAFT','Draft')
+    ('SUBMITTED','Submitted')
+    ('REVIEWED''Reviewed')
+    ('APPROVED','Approved')
+   )
