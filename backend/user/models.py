66ee5322a65d934e3ebb3f3b126908bbee79b632
bryant337 <oketayotbrian810@gmail.com>
2026-03-25 16:37:27 +0300

    32

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'admin', 'Admin'
+        INTERN = 'intern', 'Intern'
+        SUPERVISOR = 'supervisor', 'Supervisor'
+
+    class Gender(models.TextChoices):
+        MALE = 'male', 'Male'
+        FEMALE = 'female', 'Female'
+        OTHER = 'other', 'Other'
+        PREFER_NOT_TO_SAY = 'prefer_not_to_say', 'Prefer not to say'
+    # username = None  # Remove the username field from the default AbstractUser  
+
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    department = models.CharField(max_length=50, blank=True)
+
+    
