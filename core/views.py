366ba5d617e760f9c1449a04f12e30e0ebffcdae
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-03-25 12:07:34 +0300

    Update

--- core/views.py
+++ core/views.py
@@ -5,7 +5,25 @@ from .models import WeeklyLog
 from .serializers import WeeklyLogSerializer # Ensure you have this in serializers.py
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import MyTokenObtainPairSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+class ApproveLogView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def patch(self, request, pk):
+        # 1. Security Check: Is the user actually a Supervisor?
+        if request.user.role != 'SUPERVISOR':
+            return Response({"error": "Unauthorized"}, status=status.HTTP_403_FORBIDDEN)
+        
+        try:
+            log = WeeklyLog.objects.get(pk=pk)
+            log.status = 'APPROVED' # The "Divine Stamp"
+            log.save()
+            return Response({"message": "Log approved successfully!"}, status=status.HTTP_200_OK)
+        except WeeklyLog.DoesNotExist:
+            return Response({"error": "Log not found"}, status=status.HTTP_404_NOT_FOUND)
 class WeeklyLogListCreateView(generics.ListCreateAPIView):
     serializer_class = WeeklyLogSerializer
     permission_classes = [permissions.IsAuthenticated]
