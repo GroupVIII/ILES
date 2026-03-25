c8ec3d425229de30c1b35615ff7e7d1cedc398aa
bryant337 <oketayotbrian810@gmail.com>
2026-03-25 19:04:36 +0300

    model

--- backend/evaluation/models.py
+++ backend/evaluation/models.py
@@ -2,11 +2,12 @@ from datetime import timezone
 from django.db import models
 import uuid
 from django.utils import timezone
-from user.models import User
+from user.models import CustomUser
 # import timezone
 
 # Create your models here.
 class Evaluation(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='evaluations')
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
