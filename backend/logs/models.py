6670a992b3f6d19565657ba58b0e0e18b307cd2f
bryant337 <oketayotbrian810@gmail.com>
2026-03-25 13:50:56 +0300

    log

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from user.models import User
+
+class Log(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return f"{self.user.username} - {self.action} at {self.timestamp}"
