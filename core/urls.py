2b4be2d9c21401c6ed8b79f86cb1768f056aeb30
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-03-25 12:09:21 +0300

    update

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('api/issues/', IssueListCreateView.as_view(), name='issue-list'),
     path('api/all-logs/', views.SupervisorLogListView.as_view(), name='all-logs'),
+    path('api/logs/<int:pk>/approve/', views.ApproveLogView.as_view(), name='approve-log'),
 ]
