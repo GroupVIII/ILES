4d4e0f457153283c1f3373f57b2bbbd0751f4592
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-03-25 12:03:20 +0300

    Update

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import IssueListCreateView
+from . import views
 
 urlpatterns = [
     path('api/issues/', IssueListCreateView.as_view(), name='issue-list'),
+    path('api/all-logs/', views.SupervisorLogListView.as_view(), name='all-logs'),
 ]
