b4f51ee5124c590d7d82e0f200f15dc3d7b3e5f5
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-03-27 08:46:07 +0300

    Add models file and import models

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -1,11 +1,9 @@
+#logs/models.py
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
+from django.conf import BaseModel
+from core.models import BaseModel
+from accounts.models import User
+import os
 
-# Create your models here.
-class Weeklylog(models.Model):
-   STATUS_CHOICES = ( 
-      ('DRAFT','Draft')
-    ('SUBMITTED','Submitted')
-    ('REVIEWED''Reviewed')
-    ('APPROVED','Approved')
-   )
-   
