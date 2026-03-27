679708035d94c57f44a2d91e50edc15718368ef0
ritah <aromorachritah@gmail.com>
2026-03-27 11:29:17 +0300

    Create evalaution app

--- evaluations/models.py
+++ evaluations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.utils import timezone
+from core.models import BaseModel
+from accounts.models import User
+from reports.models import WeeklyReport
 
 # Create your models here.
