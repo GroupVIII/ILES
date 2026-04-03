d5b6f427f1f1a0757159d4bf1c03eae4261ac96b
bryant337 <oketayotbrian810@gmail.com>
2026-04-03 13:01:26 +0300

    12321

--- iles_frontend/src/App.jsx
+++ iles_frontend/src/App.jsx
@@ -1,2 +1,17 @@
 import { useState, setState } from "react";
-import '';
+import Dashboard from "./pages/Dashboard.jsx";
+import './App.css';
+import LoginPage from "./pages/LoginPage.jsx";
+
+// function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//     return (   
+//         <div className="App">
+//             {isLoggedIn ? (
+//                 <h1>Welcome to the Dashboard!</h1>
+//             ) : (
+//                 <LoginPage onLogin={() => setIsLoggedIn(true)} />
+//             )}
+//         </div>
+//     );
