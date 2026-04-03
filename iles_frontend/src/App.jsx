acf03d688f2ef6d31bd89fd4fb6ba914d9c7823f
bryant337 <oketayotbrian810@gmail.com>
2026-04-03 13:02:17 +0300

    qwqe

--- iles_frontend/src/App.jsx
+++ iles_frontend/src/App.jsx
@@ -3,8 +3,8 @@ import Dashboard from "./pages/Dashboard.jsx";
 import './App.css';
 import LoginPage from "./pages/LoginPage.jsx";
 
-// function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 //     return (   
 //         <div className="App">
