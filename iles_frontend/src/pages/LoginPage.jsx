ab356160b247f0d12375cd0de2d89a9017aa5214
bryant337 <oketayotbrian810@gmail.com>
2026-04-03 14:42:55 +0300

    wecome

--- iles_frontend/src/pages/LoginPage.jsx
+++ iles_frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import './LoginPage.css';
+// import './LoginPage.css';
 import axios from 'axios';
 
 function LoginPage(){
