9507a8ad2f3a7fbdcdedbea317632e8181fb413b
bryant337 <oketayotbrian810@gmail.com>
2026-04-03 12:07:20 +0300

    122

---
+++ iles_frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,34 @@
+// import { useState } from "react";
+
+// function LoginPage(){
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+
+
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <input
+//              type="email"
+//              value={email}
+//              onChange={(e)=>setEmail(e.target.value)}
+//              placeholder="Email"
+
+             
+
+//             />
+
+//             <input
+//                 type="password"
+//                 value={password}
+//                 onChange={(e)=>setPassword(e.target.value)}
+//                 placeholder="Enter Password"
+
+//             />
+
+//             <button type="submit" diabled={!email || !password}>
+//             Login
+//             </button>
+
+//         </form>
+//     );
+// }
