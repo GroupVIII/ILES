ef45d7a87471b27c7012e51411c487d469f6f167
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-04 15:26:32 +0300

    added category choices

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -39,6 +39,7 @@ class Category(models.TextChoices):
     title = models.CharField(max_length=200)
     description = models.TextField()
     category = models.CharField(max_length=20)
+    choices = category.choices
 
 
 
