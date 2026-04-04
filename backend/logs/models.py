f3b6c13a093c84546592d4e4b4c051c1caf018af
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-04 16:21:35 +0300

    added tags field

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -41,6 +41,7 @@ class Category(models.TextChoices):
     category = models.CharField(max_length=20)
     choices = category.choices
     default = category.DEVELOPMENT
+    tags = models.JSONField(default=list)
     
 
 
