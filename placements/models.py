e74453c1911caf44db9f18b2d5e1b63140f2fbff
ritah <aromorachritah@gmail.com>
2026-04-07 15:06:05 +0300

    placement code

--- placements/models.py
+++ placements/models.py
@@ -1,3 +1,24 @@
+# placements/models.py
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.utils import timezone
+from core.models import BaseModel
+from accounts.models import User
+
+class Department(BaseModel):
+    """Departments within the organization."""
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+
+    # Manager/Head of Department
+    head = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='headed_departmets',
+        limit_choices_to={'role__in' :[User.Roles.SUPERVISOR,User.Roles.ADMIN]}'
+    )
 
 # Create your models here.
