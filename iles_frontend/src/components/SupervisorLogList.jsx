2e5f34ab9babb0529032c636fa2086ec4bbdc843
bryant337 <oketayotbrian810@gmail.com>
2026-04-07 16:28:05 +0300

    eww

--- iles_frontend/src/components/SupervisorLogList.jsx
+++ iles_frontend/src/components/SupervisorLogList.jsx
@@ -6,7 +6,11 @@ function SupervisorLogList() {
     const [logs, setLogs] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
+    return (
+        <div>
+            <h1>Supervisor Log List</h1>
+        </div>
+    )
 }
 export default SupervisorLogList;
 
