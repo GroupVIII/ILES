206415a5e7f024469013cb866b6c460e3c67ebf5
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-07 10:18:06 +0300

    addede JSONField 'tags' for storing array of tags

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -41,7 +41,7 @@ class Category(models.TextChoices):
     category = models.CharField(max_length=20)
     choices = category.choices
     default = category.DEVELOPMENT
-    tags = models.JSONField(default=list)
+    tags = models.JSONField(default=list, blank=True)
     
 
 
