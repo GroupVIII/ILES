1e188651c7bbb3ec81285f5b7ac920c191c731d3
bryant337 <oketayotbrian810@gmail.com>
2026-04-07 16:24:23 +0300

    super

--- iles_frontend/src/components/SupervisorLogList.jsx
+++ iles_frontend/src/components/SupervisorLogList.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 // import axios from 'axios';
 
-function SupervisorLogList(){
-    
+function SupervisorLogList() {
+    const [logs, setLogs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
 }
+export default SupervisorLogList;
+
+
