4c1df6868fec578c132620fe2d928c1162232d70
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-04-09 16:12:59 +0300

    +

--- backend/urls.py
+++ backend/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include # You MUST import 'include'
-from rest_framework_simplejwt.views import TokenRefreshView
-from core.views import MyTokenObtainPairView # Import your custom view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Point the login to your custom view for the Role check
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # This line connects the two files! 
-    # It tells Django: "Anything starting with api/ belongs to the core.urls"
+    # This is the single source of truth for all ILES API endpoints
     path('api/', include('core.urls')), 
 ]
