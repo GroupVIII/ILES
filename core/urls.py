68f46b0edd68b5bff7cddadd7396c362dc2738c8
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-04-09 16:13:44 +0300

    +

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,11 @@ from . import views
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    # Identity & Access (Crucial for Login)
+    # Identity & Access
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # Features
+    # ILES Features
     path('issues/', views.IssueListCreateView.as_view(), name='issue-list'),
     path('logs/', views.WeeklyLogListCreateView.as_view(), name='log-list'),
     path('all-logs/', views.SupervisorLogListView.as_view(), name='all-logs'),
