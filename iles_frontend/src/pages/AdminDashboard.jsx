6adf906f243bb8ad7ca50be549a64a1b718fc4f3
bryant337 <oketayotbrian810@gmail.com>
2026-04-09 11:27:29 +0300

    hello

---
+++ iles_frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function AdminDashboard(){
+
+return(
+    <div>
+        <h2>Admin Dashboard</h2>
+    </div>
+);
+}
+export default AdminDashboard();
---
+++ iles_frontend/src/pages/SupervisorDashboard.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Supervisordashboard() {
+
+    return (
+        <div>
+            <h2>Supervisor Dashboard</h2>
+        </div>
+    );
+}
+export default Supervisordashboard();
