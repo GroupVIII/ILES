2076be63c530cd32df12fb82bde8d96f1453a891
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-04-09 16:12:59 +0300

    +

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    # Identity & Access (Crucial for Login)
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Features
     path('issues/', views.IssueListCreateView.as_view(), name='issue-list'),
     path('logs/', views.WeeklyLogListCreateView.as_view(), name='log-list'),
     path('all-logs/', views.SupervisorLogListView.as_view(), name='all-logs'),
