8f8460e82ef69364343b2ba9122e8591b9d2518b
bryant337 <oketayotbrian810@gmail.com>
2026-04-09 11:27:29 +0300

    hello

--- iles_frontend/src/pages/Dashboard.jsx
+++ iles_frontend/src/pages/Dashboard.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 // import Issue from "../components/Issue.jsx";
+import AdminDashboard from "./AdminDashboard";
+import StudentDashboard from "./StudentDashboard";
+import SupervisorDashboard from "./SupervisorDashboard";
 
 
 
 
 function Dashboard() {
-    const [isExpanded, setIsExpanded] = useState(true);
-    const toggleExpand = () => {
-        setIsExpanded(!isExpanded);
-    };
-    
+  const [isExpanded, setIsExpanded] = useState(true);
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
 
   return (
     <div className="intro">
       <h1>Welcome to the Dashboard!</h1>
     </div>
-    
+
   );
 }
 
---
+++ iles_frontend/src/pages/StudentDashboard.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function StudentDashboard() {
+
+    const [] = useState('');
+
+    return (
+        <div>
+            <h2>Student Dashbord</h2>
+        </div>
+    );
+}
+export default StudentDashboard();
