c463b180eca252cd36cfa4bfaa43320e1272fd73
bryant337 <oketayotbrian810@gmail.com>
2026-04-11 13:02:32 +0300

    feat: implement core user, evaluation, and logging models with associated API views and routing

---
+++ backend/user/urls.py
@@ -0,0 +1,9 @@
+
+from user.views import CustomUserViewSet
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter
+router.register('users', CustomUserViewSet)
+
+urlpatterns = router.urls
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from user.models import CustomUser
+from user.serializers import CustomUserSerializers
 # Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializers
+    permission_class = ['rest_framework.permissions.IsAuthenticated']
