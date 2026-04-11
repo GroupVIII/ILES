492cdcadc2f2535bb04755b693d7eda0198b8448
bryant337 <oketayotbrian810@gmail.com>
2026-04-11 13:37:52 +0300

    feat: initialize Django project settings with PostgreSQL, JWT authentication, and CORS configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^y+pv(-pdyq2dyv7%l-jughju*t&xyy%-4-c(u7owcc24n)o=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
