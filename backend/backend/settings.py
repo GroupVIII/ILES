f876345da078c957d87a4e3c8e4a8ee01e8db2e2
bryant337 <oketayotbrian810@gmail.com>
2026-04-11 14:18:49 +0300

    feat: initialize user app routes with CustomUserViewSet using DefaultRouter

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
 ]
 
 AUTH_USER_MODEL = "user.CustomUser"
