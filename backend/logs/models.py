b0a67fd84a2d4d909b68dd777ce26df1c051c39f
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-13 19:39:25 +0300

    added content type field

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -134,6 +134,7 @@ def clean(self):
                         )
                         filename = models.CharField(max_length=255)
                         file_size = models.IntegerField(help_text="File size in bytes")
+                        content_type = models.CharField(max_length=100)
                         
 
 
