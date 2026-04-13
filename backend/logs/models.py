7fee07ca2fe9274bcf1ade27c3dbb5db782f0a5d
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-13 20:02:32 +0300

    created TimeOff model

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -144,7 +144,9 @@ def clean(self):
                               if self.file and not self.filename:
                                   self.filename = os.path.basename(self.file.name)
                             super().save(*args,**kwargs) 
-                              
+                            class TimeOff(BaseModel):
+                                
+
 
 
 
