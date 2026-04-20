9d286565b2fe1e27e9f5922db7ef5604f6f53dd4
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-20 16:33:18 +0300

    index status field

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -167,6 +167,7 @@ def clean(self):
         reason = models.TextField()
         status = models.CharField(max_length=20,choices=Status.choices)
         default = Status.PENDING
+        db_index=True
         
 
 
