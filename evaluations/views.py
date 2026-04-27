f82932d95c7663d230a4988afad709850b7c1ead
ritah <aromorachritah@gmail.com>
2026-04-27 22:01:18 +0300

    feat: set up initial DRF view imports and serializers

--- evaluations/views.py
+++ evaluations/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+#create your views here.
+#evaluations/views.py
 
-# Create your views here.
+from rest__framework import viewsets, permissions, status
+from rest__framework.decorators import action
+from rest__framework.response import Response
+from django.utils import timezone
+from django.db.models import Q
+from .models import EvaluationRubric, Evaluation, EvaluationGoal, EvaluationSkill, EvaluationReminder
+from .serializers import (
+    EvaluationRubicSerializer, EvaluationSerializer,
+    EvaluationGoalSerializer, EvaluationStillSerializer,
+    EvaluationReminderSerializer
+)
+import logging
+
+
+
