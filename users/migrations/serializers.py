ff16aa18efaa65688abf196c4039029f4f97ea7f
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-04-29 09:51:12 +0300

    LoginSerializer

--- users/migrations/serializers.py
+++ users/migrations/serializers.py
@@ -41,3 +41,45 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data['role'] = self.user.role
         
         return data
+
+class LoginSerializer(serializers.Serializer):
+    """Serializer for user login"""
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+        
+        if email and password:
+            user = authenticate(
+                request=self.context.get('request'),
+                username=email,
+                password=password
+            )
+            
+            if not user:
+                raise serializers.ValidationError(
+                    "Unable to log in with provided credentials.",
+                    code='authorization'
+                )
+            
+            if not user.is_active:
+                raise serializers.ValidationError(
+                    "User account is disabled.",
+                    code='authorization'
+                )
+            
+            if user.is_account_locked():
+                raise serializers.ValidationError(
+                    f"Account is locked until {user.locked_until}",
+                    code='authorization'
+                )
+        else:
+            raise serializers.ValidationError(
+                'Must include "email" and "password".',
+                code='authorization'
+            )
+        
+        data['user'] = user
+        return data
