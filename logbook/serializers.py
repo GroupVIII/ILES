5892227db3937fcd807318ca94eb1be04397b5a8
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-29 12:14:14 +0300

    created class

--- logbook/serializers.py
+++ logbook/serializers.py
@@ -3,3 +3,6 @@ from django.utils import timezone
 from .models import LogEntry, LogAttachment, TimeOff
 from accounts.serializers import UserSerializer
 from core.api import BaseModelSerializer
+
+class LogAttachmentSerializer(BaseModelSerializer):
+    
