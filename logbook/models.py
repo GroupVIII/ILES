baa81e9ac8cfa7e60d66fda4642ad4aa2737eebd
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-29 10:35:33 +0300

    added BaseModel, User model and os import

--- logbook/models.py
+++ logbook/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from gjango.utils import timezone
 from django.conf import settings
+from core.models import BaseModel
+from accounts.models import User
+import os 
