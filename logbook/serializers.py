e4c60fab7906fad707c8cc3a2250ac3373873e1e
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-04-29 11:59:17 +0300

    imported BaseModelSerializer

--- logbook/serializers.py
+++ logbook/serializers.py
@@ -2,3 +2,4 @@ from rest_framework import serializers
 from django.utils import timezone
 from .models import LogEntry, LogAttachment, TimeOff
 from accounts.serializers import UserSerializer
+from core.api import BaseModelSerializer
