309543c1ef6b03be7d04331eba0cc63a84027bb6
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-05-02 13:46:02 +0300

    added max validator for hours

--- logbook/models.py
+++ logbook/models.py
@@ -39,8 +39,8 @@ class LogEntry(BaseModel):
     start_time = models.TimeField()
     end_time = models.TimeField()
     hours = models.DecimalField(max_digits=4, decimal_places=2)
-    validators = [MinValueValidator(0.25)]
-    
+    validators = [MinValueValidator(0.25), MaxValueValidator(24)]
+
 
 
 
