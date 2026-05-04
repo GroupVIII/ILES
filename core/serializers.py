a2c638321c34f4d0f31680104753e5dea910625e
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-05-04 10:17:25 +0300

    +

--- core/serializers.py
+++ core/serializers.py
@@ -1,2 +1,4 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import CustomUser, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation
+from django.contrib.auth.hashers import make_password
