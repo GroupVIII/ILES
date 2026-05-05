858c4ce8d7823526d5983b36ffb3305593a109e1
ritah <aromorachritah@gmail.com>
2026-05-05 19:30:44 +0300

    Configure drf_yasg schema view for internshio System API

--- config/urls.py
+++ config/urls.py
@@ -7,3 +7,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# API documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Internship System API",
+
