7b3713071ae45c669b1c286c0ab25bebe3cc7284
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-05-05 17:36:22 +0300

    addded category field

--- logbook/serializers.py
+++ logbook/serializers.py
@@ -67,7 +67,7 @@ class LogEntryUpdateSerializer(BaseModelSerializer):
         model = LogEntry
         fields = [
             'start_time', 'end_time', 'hours', 'title', 'description',
-             
+             'category', 
         ]
         
 
