b142cd97dc58f0775fe491037e512b1ee0109b72
ritah <aromorachritah@gmail.com>
2026-05-05 22:46:37 +0300

    feat: add email notification utility with logging

--- core/utils.py
+++ core/utils.py
@@ -25,3 +25,8 @@ def validate_email(email):
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(pattern, email) is not None
 
+def send_email_notification(subject, message, recipient_list, html_message=None):
+    """
+    Send email notification with logging.
+    """
+
