4a902907c8eecfbd2370fb33ee376da052e61a8d
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-05-06 12:48:51 +0300

    created LogAttachmentInline admin class

--- logbook/admin.py
+++ logbook/admin.py
@@ -1,5 +1,9 @@
+# logs/admin.py
 from django.contrib import admin
-from .models import LogEntry
-# Register your models here.
+from .models import LogEntry, LogAttachment, TimeOff
+
+
+
+
 
 admin.site.register(LogEntry)
