a95c6f84c9375258891f5c2a3a0668c6e3bc9337
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-05-06 14:53:16 +0300

    implemented logbook views with serializers and REST framework

--- logbook/views.py
+++ logbook/views.py
@@ -1,1 +1,20 @@
-from rest_framework import 
+# logs/views.py
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils import timezone
+from django.db import models
+from django.db.models import Q, Sum
+from .models import LogEntry, LogAttachment, TimeOff
+from .serializers import (
+    LogEntrySerializer, LogEntryCreateSerializer, LogEntryUpdateSerializer,
+    LogEntryReviewSerializer, TimeOffSerializer, TimeOffCreateSerializer,
+    TimeOffReviewSerializer, LogAttachmentSerializer
+)
+from accounts.models import User
+from core.api import MultiSerializerViewSet
+from notification.services import NoticationsService, NoticationTemplates
+import logging
+
+
+
