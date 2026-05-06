6e863978951671182533cc010593005e76e99d9f
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-05-06 13:27:27 +0300

    updated urlpatterns to include router urls for timeoff views

--- logbook/urls.py
+++ logbook/urls.py
@@ -7,6 +7,11 @@ router = DefaultRouter()
 router.register(r'entries', views.LogEntryViewSet, basename='logentry')
 router.register(r'timeoff', views.TimeOffViewSet, basename='timeoff')
 
+urlpatterns = [
+    path('', include(router.urls))
+]
+
+
 
 
 
