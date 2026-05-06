a968dda0f3dcc5fc89b9895434ec8fb352b8556f
ssekiziyivumaria <ssekiziyivumaria@gmail.com>
2026-05-06 12:54:29 +0300

    set LogAttachment as the model LogAttachmentInline admin

--- logbook/admin.py
+++ logbook/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from .models import LogEntry, LogAttachment, TimeOff
 
 
-
+class LogAttachmentInline(admin.TabularInline):
+    model = LogAttachment
 
 
 admin.site.register(LogEntry)
