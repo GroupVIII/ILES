f92dbee254c6bf9d1caa970b89adb746dfcbba87
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-05-06 10:19:30 +0300

    +

--- core/views.py
+++ core/views.py
@@ -1,4 +1,12 @@
-
+from rest_framework import viewsets, permissions, status, generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Count
+from .models import CustomUser, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation, Notification
+from .serializers import (UserSerializer, InternshipPlacementSerializer, WeeklyLogSerializer, EvaluationCriteriaSerializer, EvaluationSerializer, NotificationSerializer)
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
+from rest_framework.views import APIView
 
 class UserViewSet(viewsets.ModelViewSet):
     """
