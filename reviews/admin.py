16f2fee1f69372bcb79623a865af1994ba6a2f10
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-05-07 08:41:56 +0300

    ReportCommentInline

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
+# reviews/admin.py
 from django.contrib import admin
+from .models import WeeklyReport, ReportComment, ReportTemplate, ReportReminder
 
-# Register your models here.
+
+class ReportCommentInline(admin.TabularInline):
+    model = ReportComment
+    extra = 0
+    readonly_fields = ('created_at',)
+
