7a4ab40d8985c820af401da6c44cab87c4a14fdd
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-05-07 09:01:31 +0300

    WeeklyReport

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
+# reports/models.py
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
+from core.models import BaseModel
+from accounts.models import User
+from logs.models import LogEntry
 
-# Create your models here.
+
+class WeeklyReport(BaseModel):
+    """
+    Weekly summary report created by interns.
+    """
+    
