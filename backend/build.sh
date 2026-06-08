328027a9e352463b664339c0beb7e4d4b0f60902
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-06-08 15:26:41 +0300

    +

--- backend/build.sh
+++ backend/build.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Build React
+# 1. Install Python dependencies first!
+pip install -r requirements.txt
+
+# 2. Build frontend
 cd ../frontend
 npm install
 npm run build
-
-# 2. Copy the build output to a place Django can see
-# 'dist' is the standard output folder for Vite
-# We copy everything to backend/staticfiles
-cp -r dist/* ../backend/staticfiles/
-
-# 3. CRITICAL: Ensure index.html is also in the root of templates
-# if you used a 'templates' folder instead
-cp dist/index.html ../backend/staticfiles/
-
 cd ../backend
 
-# 4. Collect static files
+# 3. Now collect static and migrate
 python manage.py collectstatic --no-input
 python manage.py migrate
