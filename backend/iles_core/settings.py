b23c7729ac2a4bba7280e07ccac1f2f756e017e6
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-06-08 13:58:38 +0300

    staticfiles

--- backend/iles_core/settings.py
+++ backend/iles_core/settings.py
@@ -97,7 +97,10 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../frontend/dist/assets'), # Adjust path to your build output
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
