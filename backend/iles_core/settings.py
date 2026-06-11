d553563f37c52f4265d6845712d5073fe83a42c3
joshuassenyonjo1-dev <joshuassenyonjo1@gmail.com>
2026-06-11 08:41:58 +0300

    netlify

--- backend/iles_core/settings.py
+++ backend/iles_core/settings.py
@@ -111,7 +111,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom Auth User and CORS Settings
 AUTH_USER_MODEL = 'api.CustomUser'
 
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = [
+    "https://projectilessystem.netlify.app/", # Replace with your actual Netlify URL
+    "http://localhost:5173",
+]
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
