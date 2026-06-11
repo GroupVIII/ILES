afd6b4ef0011f355e597a70d1f25ccda57cba3cd
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-06-11 10:06:16 +0300

    add changes

--- evaluations/views.py
+++ evaluations/views.py
@@ -16,7 +16,7 @@ from .serializers import (
 import logging
  logger = logging.getLogger(__name__)
 
-class EvaluationRubricViewSet(viewsets.ModelViewSet):
+ class EvaluationRubricViewSet(viewsets.ModelViewSet):
     """ViewSet for managing evaluation rubrics."""
     queryset = EvaluationRubric.objects.all()
     serializer_class = EvaluationRubicSerializer
