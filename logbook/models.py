bd7b9465d4f829e1e18ef0d3d0cfa33a1bcc3b04
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-06-11 10:00:01 +0300

    add changes

--- logbook/models.py
+++ logbook/models.py
@@ -20,15 +20,7 @@ class LogEntry(BaseModel):
         REJECTED = 'rejected','Rejected'
         NEEDS_REVISION = 'needs_revision','Needs_Revision'
 
-    class Category(models.TextChoices):
-        DEVELOPMENT = 'development', 'Development'
-        DESIGN = 'design', 'Design'
-        RESEARCH = 'research', 'Research'
-        MEETING = 'meeting', 'Meeting'
-        DOCUMENTATION = 'documentation', 'Documentation'
-        TRAINING = 'training', 'Training'
-        ADMIN = 'admin', 'Administrative'
-        OTHER = 'other', 'Other'
+    
 
     user = models.ForeignKey(
         User,
