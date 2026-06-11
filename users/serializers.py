9f81db657e81abb153dbda37ff1b6db50696264f
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-06-11 09:31:45 +0300

    add changes

--- users/serializers.py
+++ users/serializers.py
@@ -14,7 +14,7 @@ class UserProfileSerializer(BaseModelSerializer):
         fields = [
             'id', 'emergency_contact_name', 'emergency_contact_phone',
             'emergency_contact_relation', 'address_line1', 'address_line2',
-            'city', 'state', 'country', 'postal_code', 'skills',
+            'city', 'country', 'skills',
             'interests', 'certifications', 'linkedin_url', 'github_url',
             'portfolio_url', 'resume', 'created_at', 'updated_at'
         ]
