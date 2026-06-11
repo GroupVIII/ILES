49148f9027abb5bb8034632c58644dd05498e1eb
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-06-11 10:04:57 +0300

    add changes

--- evaluations/serializers.py
+++ evaluations/serializers.py
@@ -3,7 +3,10 @@ from rest_framework import serializers
 from .models import EvaluationRubric,Evaluation, EvaluationGoal, EvaluationSkill, EvaluationReminder
 from accounts.serializers import UserSerializer
 
-
+class EvaluationRubricSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EvaluationRubric
+        fields = '__all__'
 
 class EvaluationSerializer(serializer.ModelSerializer):
     intern_details = UserSerializer(source='intern', read_only=True)
