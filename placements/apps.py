f18c1b35a009d60ae8da6133c3f84b6a6ac9d8ac
ANZOA JOSEPHINE CHONO <chonoanzoajosephine@gmail.com>
2026-06-12 19:49:21 +0300

    add changes

--- placements/apps.py
+++ placements/apps.py
@@ -2,7 +2,3 @@
 from django.apps import AppConfig
 
 
-class PlacementsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'placements'
-    verbose_name = 'Placements'
